df3353f680910956740ee87d88a886843572872a
loewey <grandmasterlouis@gmail.com@>
2025-10-26 20:18:59 +0000

    Changed S3 bucket used

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 provider "aws" {
-  region = "eu-west-2" 
+  region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraformBucketTest" {
-  bucket = "terraform-bucket-test-1234567890"
+resource "aws_s3_bucket" "current_bucket" {
+  bucket = "tester-devsecops-bucket"
+}
+terraform {
+  backend "s3" {
+    bucket         = "tester-devsecops-bucket"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
 }
